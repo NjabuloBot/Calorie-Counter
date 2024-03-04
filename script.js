e1f656f64abfa076cc29be3ebdbd494fb2fdba94
Njabulo <nnshiwu@gmail.com>
2024-03-04 09:22:19 +0200

    Declared a breakfastNumberInputs variable, and gave it the value of calling document.querySelectorAll() with the selector breakfast input[type=number]. This will return any number inputs that are in the #breakfast element.

--- script.js
+++ script.js
@@ -47,5 +47,5 @@ function getCaloriesFromInputs(list){
 function calculateCalories(e){
    e.preventDefault();
    isError = false;
-
+   const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
 }
