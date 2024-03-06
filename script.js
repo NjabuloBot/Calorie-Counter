4e469c7d52812b325779c8611f44c91875668b8f
Njabulo <nnshiwu@gmail.com>
2024-03-06 08:06:00 +0200

    Added a second p element with the text consumedCalories Calories Consumed and a third with the text exerciseCalories Calories Burned

--- script.js
+++ script.js
@@ -70,5 +70,7 @@ function calculateCalories(e){
    output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">
    ${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
    <hr></hr>
-   <p>${budgetCalories} Calories Budgeted</p>`;
+   <p>${budgetCalories} Calories Budgeted</p>
+   <p>${consumedCalories} Calories Consumed</p>
+   <p>${exerciseCalories} Calories Burned</p>`;
 }
