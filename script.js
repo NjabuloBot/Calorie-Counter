b4c08cf49fc988df1b67e78b999d794416ae5db9
Njabulo <nnshiwu@gmail.com>
2024-03-06 08:03:21 +0200

    Created a p element with the text budgetCalories Calories Budgeted, using interpolation to replace budgetCalories with the appropriate variable.

--- script.js
+++ script.js
@@ -67,5 +67,8 @@ function calculateCalories(e){
    const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
    const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
    const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
-   output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span><hr></hr>`;
+   output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">
+   ${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+   <hr></hr>
+   <p>${budgetCalories} Calories Budgeted</p>`;
 }
